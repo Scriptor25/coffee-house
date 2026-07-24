053cea83d5611679286b1ffd6594b796b2d73956
Scriptor25 <f.schreiber.2006@gmail.com>
2026-07-24 13:53:30 +0200

    removed index db from git tree, index file system for media contents, get media entry from database

--- src/main/kotlin/dev/scriptor/Main.kt
+++ src/main/kotlin/dev/scriptor/Main.kt
@@ -1,18 +1,102 @@
 package dev.scriptor
 
 import dev.scriptor.server.scan
+import java.io.File
 import java.sql.DriverManager
-import java.util.logging.Logger
+import java.sql.Timestamp
+import java.util.logging.*
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 fun main() {
     val log = Logger.getLogger("dev.scriptor")
+    log.level = Level.INFO
 
-    val connection = DriverManager.getConnection("jdbc:sqlite:./index.db")
+    val handler = ConsoleHandler()
+    handler.level = log.level
+    handler.formatter = object : Formatter() {
+
+        override fun format(record: LogRecord?): String? {
+            if (record == null) return null
+
+            return "[${record.level}][${record.instant}][${record.sourceClassName}.${record.sourceMethodName}(...)] ${record.message}\n"
+        }
+    }
+
+    log.useParentHandlers = false
+    log.addHandler(handler)
+
+    val connection = DriverManager.getConnection("jdbc:sqlite:index.db")
 
     val server = scan(log, "0.0.0.0", 8080, "dev.scriptor")
 
-    server.registerValue("log", log)
-    server.registerValue("connection", connection)
+    server.inject("log", log)
+    server.inject("connection", connection)
+
+    connection.prepareStatement(
+        """
+        create table if not exists media (
+            id string primary key,
+            path string unique not null,
+            modified timestamp not null
+        )
+        """.trimIndent()
+    ).use { it.execute() }
+
+    val extensions = arrayOf("mkv", "mp4")
+    val walk = File("/home/felix/Videos").walkTopDown()
+
+    val modified = Timestamp(System.currentTimeMillis())
+
+    connection.prepareStatement(
+        """
+        insert into media (id, path, modified)
+        values (?, ?, ?)
+        on conflict(path)
+        do update
+        set modified = excluded.modified
+        """.trimIndent()
+    ).use {
+        for (file in walk) {
+            if (file.extension !in extensions) continue
+
+            val id = Uuid.generateV7()
+
+            it.setString(1, id.toHexDashString())
+            it.setString(2, file.absolutePath)
+            it.setTimestamp(3, modified)
+
+            it.addBatch()
+        }
+
+        it.executeLargeBatch()
+    }
+
+    connection.prepareStatement(
+        """
+        delete from media
+        where not modified = ?
+        """.trimIndent()
+    ).use {
+        it.setTimestamp(1, modified)
+        it.executeUpdate()
+    }
+
+    connection.prepareStatement(
+        """
+        select id, path from media
+        """.trimIndent()
+    ).use { statement ->
+        statement.executeQuery().use {
+            while (it.next()) {
+                val id = it.getString(1)
+                val path = it.getString(2)
+
+                log.info("media $id -> $path")
+            }
+        }
+    }
 
     server.start()
 
--- src/main/kotlin/dev/scriptor/rest/MediaRest.kt
+++ src/main/kotlin/dev/scriptor/rest/MediaRest.kt
@@ -1,35 +1,71 @@
 package dev.scriptor.rest
 
+import dev.scriptor.model.MediaModel
 import dev.scriptor.server.annotation.*
 import dev.scriptor.server.http.result.HTTPResult
 import dev.scriptor.server.http.result.HTTPResultChannel
+import dev.scriptor.server.http.result.HTTPResultVoid
 import java.nio.channels.FileChannel
 import java.nio.file.Path
+import java.sql.Connection
 import java.util.logging.Logger
 import kotlin.time.Clock.System.now
+import kotlin.time.toKotlinInstant
+import kotlin.uuid.Uuid
 
 @Endpoint("/media")
 class MediaRest {
 
     @Inject("log")
-    var log: Logger = Logger.getGlobal()
+    lateinit var log: Logger
+
+    @Inject("connection")
+    lateinit var connection: Connection
 
     data class Session(
         val time: Long,
+        val sequence: Long,
         val next: Long,
     )
 
     val sessions: MutableMap<String, Session> = HashMap()
+    val cache: MutableMap<Uuid, MediaModel> = HashMap()
+
+    fun getMediaById(id: Uuid): MediaModel? {
+        if (id in cache) return cache[id]
+        val value = connection.prepareStatement(
+            """
+            select id, path, modified from media
+            where id = ?
+            limit 1
+            """.trimIndent()
+        ).use { statement ->
+            statement.setString(1, id.toHexDashString())
+            statement.executeQuery().use {
+                if (it.next())
+                    MediaModel(
+                        Uuid.parseHexDash(it.getString("id")),
+                        Path.of(it.getString("path")),
+                        it.getTimestamp("modified").toInstant().toKotlinInstant(),
+                    )
+                else null
+            }
+        }
+        if (value != null) {
+            cache[id] = value
+        }
+        return value
+    }
 
     @Resource("/[id]")
     fun getMediaById(
-        @PathParameter("id") id: String,
+        @PathParameter("id") id: Uuid,
         @QueryParameter("session") key: String?,
         @Header("Range") range: String?,
     ): HTTPResult<*> {
+        val media = getMediaById(id) ?: return HTTPResultVoid(404, "Not Found")
 
-        val path = Path.of("/home/felix/Videos/Noisestorm - Crab Rave (Official Music Video).mkv")
-        val channel = FileChannel.open(path)
+        val channel = FileChannel.open(media.path)
 
         val total = channel.size()
 
@@ -54,17 +90,21 @@ class MediaRest {
         val now = now().toEpochMilliseconds()
 
         val chunk: Long
+        val seq: Long
         if (key == null || key !in sessions) {
             chunk = 1024L * 1024L
+            seq = 0L
         } else {
-            val (time, next) = sessions[key]!!
+            val (time, sequence, next) = sessions[key]!!
 
-            if (next != begin) {
+            if (next != begin || (now - time) > 30_000L) {
                 chunk = 1024L * 1024L
+                seq = 0L
             } else {
-                val metric = maxOf(0L, minOf(800L, now - time - 200L)) / 100L + 1L
+                val metric = maxOf(0L, minOf(7L, sequence)) + 1L
 
                 chunk = metric * 1024L * 1024L
+                seq = sequence + 1L
             }
         }
 
@@ -77,7 +117,7 @@ class MediaRest {
         val limit = begin + count - 1
 
         if (key != null) {
-            sessions[key] = Session(now, begin + count)
+            sessions[key] = Session(now, seq, begin + count)
         }
 
         val headers: MutableMap<String, String> = HashMap()
