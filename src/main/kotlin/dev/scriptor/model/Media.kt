afddc244f7ef3e1a93a3d74b98f01eb6c34b1152
Scriptor25 <f.schreiber.2006@gmail.com>
2026-07-25 14:55:39 +0200

    removed unused imports

--- src/main/kotlin/dev/scriptor/model/Media.kt
+++ src/main/kotlin/dev/scriptor/model/Media.kt
@@ -1,9 +1,7 @@
 package dev.scriptor.model
 
 import java.nio.file.Path
-import java.sql.ResultSet
 import kotlin.time.Instant
-import kotlin.time.toKotlinInstant
 import kotlin.uuid.Uuid
 
 data class Media(
