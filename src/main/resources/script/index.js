8ed80de1e78709939748084477b6f3a20fe793c0
Scriptor25 <f.schreiber.2006@gmail.com>
2026-08-02 00:20:49 +0200

    generate m3u playlists from dashboard

--- src/main/resources/script/index.js
+++ src/main/resources/script/index.js
@@ -1,120 +1,142 @@
-import {fetchAPI} from "./api.js";
+import {fetchAPI} from "./api.js"
 import {buildTree, DirectoryNode, getCommonBase, MediaNode, segments} from "./tree.js"
 
 async function render() {
     const session = localStorage.getItem("session")
 
-    const loginForm = document.getElementById("login-form")
-    const mediaList = document.getElementById("media-list")
+    const loginEl = document.getElementById("login")
+    const formEl = document.getElementById("form")
 
-    loginForm.style.display = "none"
-    mediaList.style.display = "none"
+    const mediaEl = document.getElementById("media")
+    const playlistEl = document.getElementById("playlist")
+    const listEl = document.getElementById("list")
 
-    const endpoint = window.location.origin
+    loginEl.style.display = "none"
+    mediaEl.style.display = "none"
 
     if (session !== null) {
         const response = await fetchAPI("media", {method: "GET"})
 
-        if (response.ok) {
-            const fragment = window.location.hash
-            const slug = fragment.length ? segments(decodeURI(fragment.slice(1))) : []
-
-            const listener = event => {
-                event.preventDefault()
-
-                window.removeEventListener("hashchange", listener)
-
-                render().then()
-            }
-
-            window.addEventListener("hashchange", listener)
-
-            /**
-             * @type {Media[]}
-             */
-            const items = await response.json()
-
-            const base = getCommonBase(items)
-
-            let node = buildTree(items, base)
-            for (const segment of slug) {
-                if (node === null || node.name === segment) break
-                node = node.children
-                    .filter(node => node instanceof DirectoryNode)
-                    .find(node => node.name === segment) ?? null
-            }
-
-            if (node !== null) {
-                const listItems = node.children.toSorted((a, b) => a.name.localeCompare(b.name)).map(node => {
-                    const listItem = document.createElement("li")
-                    const anchor = document.createElement("a")
-                    if (node instanceof MediaNode) {
-                        /**
-                         * @type {MediaNode} node
-                         */
-                        anchor.href = encodeURI(`${endpoint}/media/stream/${node.item.id}?token=${session}`)
-                    } else {
-                        anchor.href = encodeURI(`${endpoint}/#${slug.length ? "/" + slug.join("/") : ""}/${node.name}`)
-                    }
-                    anchor.append(node.name)
-                    listItem.append(anchor)
-                    return listItem
-                })
-
-                if (slug.length) {
-                    const target = slug.slice(0, -1)
-
-                    const listItem = document.createElement("li")
-                    const anchor = document.createElement("a")
-                    anchor.href = encodeURI(`${endpoint}/#${target.length ? "/" + target.join("/") : ""}`)
-                    anchor.append("..")
-                    listItem.append(anchor)
-                    listItems.unshift(listItem)
-                }
-
-                mediaList.replaceChildren(...listItems)
-            }
-
-            loginForm.style.display = "none"
-            mediaList.style.display = "block"
-        } else {
+        if (!response.ok) {
             console.log(response.status, response.statusText, await response.text())
             localStorage.removeItem("session")
 
             render().then()
+            return
         }
-    } else {
-        const listener = async (event) => {
+
+        const fragment = window.location.hash
+        const slug = fragment.length ? segments(decodeURI(fragment.slice(1))) : []
+
+        window.addEventListener("hashchange", event => {
             event.preventDefault()
 
-            const data = new FormData(event.currentTarget, event.submitter)
-
-            event.currentTarget.reset()
-
-            const response = await fetchAPI("session", {
-                method: "POST",
-                body: JSON.stringify(Object.fromEntries(data)),
-                headers: {"content-type": "application/json"},
-            }, false)
-
-            if (response.ok) {
-                const data = await response.json()
-                localStorage.setItem("session", data.token)
-            } else {
-                console.log(response.status, response.statusText, await response.text())
-                localStorage.removeItem("session")
-            }
-
-            loginForm.removeEventListener("submit", listener)
-
             render().then()
+        }, {once: true})
+
+        /** @type {Media[]} */
+        const items = await response.json()
+
+        const base = getCommonBase(items)
+
+        let node = buildTree(items, base)
+        for (const segment of slug) {
+            if (node === null || node.name === segment) break
+            node = node.children
+                .filter(node => node instanceof DirectoryNode)
+                .find(node => node.name === segment) ?? null
         }
 
-        loginForm.addEventListener("submit", listener)
+        if (node !== null) {
+            const sorted = node.children
+                .toSorted((a, b) => a.name.localeCompare(b.name))
 
-        loginForm.style.display = "block"
-        mediaList.style.display = "none"
+            const listItemEls = sorted
+                .map(node => {
+                    const listItemEl = document.createElement("li")
+                    const anchorEl = document.createElement("a")
+                    if (node instanceof MediaNode) {
+                        anchorEl.href = encodeURI(`/media/stream/${node.item.id}?token=${session}`)
+                    } else {
+                        anchorEl.href = encodeURI(`/#${slug.length ? "/" + slug.join("/") : ""}/${node.name}`)
+                    }
+                    anchorEl.append(node.name)
+                    listItemEl.append(anchorEl)
+                    return listItemEl
+                })
+
+            if (slug.length) {
+                const target = slug.slice(0, -1)
+
+                const listItemEl = document.createElement("li")
+                const anchorEl = document.createElement("a")
+                anchorEl.href = encodeURI(`/#${target.length ? "/" + target.join("/") : ""}`)
+                anchorEl.append("..")
+                listItemEl.append(anchorEl)
+                listItemEls.unshift(listItemEl)
+            }
+
+            listEl.replaceChildren(...listItemEls)
+
+            const playlist = sorted
+                .map(item => {
+                    if (!(item instanceof MediaNode)) return
+                    return item.item
+                })
+                .filter(item => !!item)
+
+            const lines = playlist.flatMap(item => {
+                const url = new URL(
+                    encodeURI(`/media/stream/${item.id}?token=${session}`),
+                    window.location.origin,
+                )
+                return [`#EXTINF:0,${item.title}`, url.toString()]
+            })
+
+            const data = `#EXTM3U\r\n${lines.join("\r\n")}`
+
+            playlistEl.addEventListener("click", async () => {
+                const blob = new Blob([data], {type: "application/x-mpegurl"})
+                const objectURL = URL.createObjectURL(blob)
+
+                const anchorEl = document.createElement("a")
+                anchorEl.href = objectURL
+                anchorEl.download = "playlist.m3u8"
+                anchorEl.click()
+                URL.revokeObjectURL(objectURL)
+            })
+        }
+
+        mediaEl.style.display = "block"
+        return
     }
+
+    formEl.addEventListener("submit", async event => {
+        event.preventDefault()
+
+        const data = new FormData(event.currentTarget, event.submitter)
+
+        event.currentTarget.reset()
+
+        const response = await fetchAPI("session", {
+            method: "POST",
+            body: JSON.stringify(Object.fromEntries(data)),
+            headers: {"content-type": "application/json"},
+        }, false)
+
+        if (!response.ok) {
+            console.log(response.status, response.statusText, await response.text())
+            localStorage.removeItem("session")
+        } else {
+            /** @type {{token: string}} */
+            const data = await response.json()
+            localStorage.setItem("session", data.token)
+        }
+
+        render().then()
+    }, {once: true})
+
+    loginEl.style.display = "block"
 }
 
 render().then()
